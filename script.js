ea5d736e38aa3d7ee6832eae4e21306b64bad012
Dheerajsonekar <dheerajsonekar2@gmail.com>
2024-10-09 11:33:11 +0530

    2

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const addTwoNumber = (a, b) => a+b;
+// const addTwoNumber = (a, b) => a+b;
 
-console.log(addTwoNumber(4, 7));
+// console.log(addTwoNumber(4, 7));
+
+const student = {
+    name:"Dheeraj",
+    class: "btech",
+    rollNO: 23,
+    passingYear: 2024
+}
+
+
+console.log(student);
