067765ba9941bc0fa7f49b3d803f1554036f7807
Dheerajsonekar <dheerajsonekar2@gmail.com>
2024-10-10 15:22:28 +0530

    2

--- script.js
+++ script.js
@@ -2,12 +2,24 @@
 
 // console.log(addTwoNumber(4, 7));
 
-const student = {
-    name:"Dheeraj",
-    class: "btech",
-    rollNO: 23,
-    passingYear: 2024
-}
+// const student = {
+//     name:"Dheeraj",
+//     class: "btech",
+//     rollNO: 23,
+//     passingYear: 2024
+// }
 
 
-console.log(student);
+// console.log(student);
+
+const fruits =  ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+const newFruits = fruits.map(fruit=>{
+    if(fruit==' '){
+        return fruit = 'emptyString';
+    }
+    else{
+        return fruit;
+    }
+})
+
+console.log(newFruits);
