1965d8f17587f21915852cf583d142e4ae1e4444
Dheerajsonekar <dheerajsonekar2@gmail.com>
2024-10-13 11:31:40 +0530

    8

--- script.js
+++ script.js
@@ -12,14 +12,46 @@
 
 // console.log(student);
 
-const fruits =  ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
-const newFruits = fruits.map(fruit=>{
-    if(fruit==' '){
-        return fruit = 'emptyString';
-    }
-    else{
-        return fruit;
-    }
+// const fruits =  ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+// const newFruits = fruits.map(fruit=>{
+//     if(fruit==' '){
+//         return fruit = 'emptyString';
+//     }
+//     else{
+//         return fruit;
+//     }
+// })
+
+// console.log(newFruits);
+
+// function printData(letter) {
+//     return new Promise((resolve, reject)=> {
+//       setTimeout(()=>{
+//         console.log(letter)
+//         resolve();
+//       }, 1000)
+//     })
+// }
+
+// async function printD(){
+//     await printData('a');
+//     await printData('b');
+//     await printData('c');
+//     await printData('d');
+//     await printData('e');
+    
+// }
+
+// printD();
+
+
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+  console.log(req);
+  res.end('successfull');
 })
 
-console.log(newFruits);
+server.listen(4000, ()=>{
+  console.log(" Hello Dheeraj sonekar! Server is running on the port 4000.");
+})
