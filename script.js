1c3f5d245273ad55cd6b47680080c673f8d0c398
Dheerajsonekar <dheerajsonekar2@gmail.com>
2024-10-17 14:59:07 +0530

    complete printing message

--- script.js
+++ script.js
@@ -9,7 +9,6 @@
 //     passingYear: 2024
 // }
 
-
 // console.log(student);
 
 // const fruits =  ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
@@ -39,11 +38,10 @@
 //     await printData('c');
 //     await printData('d');
 //     await printData('e');
-    
+
 // }
 
 // printD();
-
 
 // const http = require('http');
 
@@ -56,24 +54,82 @@
 //   console.log(" Hello Dheeraj sonekar! Server is running on the port 4000.");
 // })
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req,res)=>{
-  res.setHeader('Content-Type', 'text/plain');
-  
-  if(req.url=='/home'){
-    res.write('Welcome to home')
+// const server = http.createServer((req,res)=>{
+//   res.setHeader('Content-Type', 'text/plain');
+
+//   if(req.url=='/home'){
+//     res.write('Welcome to home')
+//   }
+//   else if(req.url=='/About'){
+//     res.write('Welcome to About us page')
+//   }
+//   else if(req.url=='/node'){
+//     res.write('Welcome to my nodejs project')
+//   }
+//   else {
+//     res.write('Welocome to this project')
+//   }
+//   res.end();
+// })
+
+// server.listen(3000);
+
+const http = require("http");
+const fs = require("fs");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    
+    let messages = "";
+    try {
+      messages = fs.readFileSync("message.txt", "utf-8");
+    } catch (err) {
+      messages = "No message in file!";
+    }
+
+    res.write("<html>");
+    res.write("<head><title>MY home page</title><head>");
+    res.write("<body>");
+    res.write(`<h3>Message:<pre>${messages}</pre></h3>`);
+    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">submit</button></form>')
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
   }
-  else if(req.url=='/About'){
-    res.write('Welcome to About us page')
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      
+     
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
-  else if(req.url=='/node'){
-    res.write('Welcome to my nodejs project')
-  }
-  else {
-    res.write('Welocome to this project')
-  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>hi</title></head>");
+  res.write("<body><h2>Lets work for the same</h2></body>");
+  res.write("</html>");
   res.end();
-})
+});
 
-server.listen(3000);
+server.listen(PORT, () => {
+  console.log("Server is running on " + PORT + ".");
+});
