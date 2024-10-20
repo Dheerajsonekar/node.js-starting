bd91cccd97da3363f29c530a8676d2a2af00e5b5
Dheerajsonekar <dheerajsonekar2@gmail.com>
2024-10-20 12:41:47 +0530

    size input

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.use('/add-product', (req, res, next)=>{
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="enter product name"><input type="text" name="size" placeholder="enter size"><button type="submit">Add Product</button></form>');
   
 })
 
